b52970f34ce9c0837ab93de726911879cedabf1e
Eva Kerrigan <evakerrigan@gmail.com>
2022-10-30 22:57:36 +0300

    feat: test 245

--- src/component/Borch/Borch.jsx
+++ src/component/Borch/Borch.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './Borch.css'
-import '../../image/21.gif'
 import johnsonImg from "../../image/110.gif"
 
 
 function Food({name, picture}) {
   return (
-  <div>
+  <div className='wrapper-3'>
     <h3>Я люблю {name}</h3>
-    <img src={picture} alt='' />
+    <img src={picture} alt='' style={{
+      width:'50px',
+      height:'50px',
+      'margin': '0 auto',
+      border:'1px solid yellow'}} />
     <div style={{
       width:'50px',
       height:'50px',
       'margin': '0 auto',
-      border:'1px solid #900',
+      border:'1px solid red',
       backgroundImage:'url(../image/21.gif)'
       }}
       ></div>
@@ -29,15 +32,15 @@ const foodILike = [
   },
   {
     'name': 'упячка2',
-    'image': 'image/41.gif'
+    'image': '/images/41.gif'
   },
   {
     'name': 'упячка3',
-    'image': '../../image/1.jpg'
+    'image': '../../image/41.gif'
   },
   {
     'name': 'упячка4',
-    'image': '/image/110.gif'
+    'image': 'borch/image/110.gif'
   }
 ]
 
@@ -49,15 +52,20 @@ export const Borch = () => {
       div style=backgroundImage</h2>
         {foodILike.map(elem => <Food name={elem.name} picture={elem.image} />)}
 
+      <h2 className='test2'>Еще какой то тест</h2>
+        {foodILike.map(elem => <Food name={elem.name} picture={`src={require(elem.image)}`} />)}
+
       <h2 className='test2'>Тест отображения картинок из файла HTML картинкой <br/>
       img src=url(../../image/21.gif)</h2>
-        <img src='url(../../image/21.gif)' alt='' style={{
+        <img src='url(/images/21.gif)' alt='' style={{
         width:'50px',
         height:'50px',
         'margin': '0 auto',
-        border:'1px solid #900',
-        backgroundImage:'url(../image/21.gif)'
+        border:'1px solid blue'
         }}/>
+
+      <h2 className='test2'>Отображение картинки с помощью require</h2>
+        <img src={require("../../image/41.gif")}></img>
 
       <h2 className='test2'>И еще тест картинки из файла стилей CSS</h2>
         <div className='test'></div>
