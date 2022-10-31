6c0a70e96bb8ad92fd7459375cd418b6c94b706c
Eva Kerrigan <evakerrigan@gmail.com>
2022-10-31 16:36:31 +0300

    feat: testing

--- src/component/ImagePath/ImagePath.jsx
+++ src/component/ImagePath/ImagePath.jsx
@@ -18,7 +18,7 @@ function Food({name, picture}) {
       height:'50px',
       'margin': '0 auto',
       border:'1px solid red',
-      backgroundImage:`url(${picture})`,
+      backgroundImage:`url(/images/21.gif)`,
       backgroundPosition: 'center',
       backgroundSize: 'cover'
       }}
@@ -54,15 +54,14 @@ export const ImagePath = () => {
   return (
     <div>
 
-      <h2 className='test2'>Тест отображения картинок из файла HTML стилями бэкграундом <br/>
-      div style=backgroundImage</h2>
-        {foodILike.map(elem => <Food name={elem.name} picture={elem.image} />)}
+      <h2 className='test2'>Тест отображения картинок из файла HTML <br/>просто относительными путями из бд</h2>
+        {foodILike.map(elem => <Food key={elem.image} name={elem.name} picture={elem.image} />)}
 
       <h2 className='test2'>Тест с process.env.PUBLIC_URL</h2>
-        {foodILike.map(elem => <Food name={elem.name} picture={`{${process.env.PUBLIC_URL}${elem.image}`} />)}
+        {foodILike.map(elem => <Food key={elem.image} name={elem.name} picture={`{${process.env.PUBLIC_URL}${elem.image}`} />)}
 
       <h2 className='test2'>Тест с require</h2>
-        {foodILike.map(elem => <Food name={elem.name} picture={`{require(${elem.image})}`} />)}
+        {foodILike.map(elem => <Food key={elem.image} name={elem.name} picture={`{require(${elem.image})}`} />)}
 
 
 
@@ -75,25 +74,25 @@ export const ImagePath = () => {
       <h2 className='test2'>Тест отображения картинок из файла HTML картинкой <br/>
       img src=url(../../image/21.gif)</h2>
         <img src='url(/images/21.gif)' alt='' style={{
-        width:'50px',
-        height:'50px',
+        width:'100px',
+        height:'100px',
         'margin': '0 auto',
         border:'1px solid blue'
         }}/>
 
-      <h2 className='test2'>Отображение картинки с помощью process.env.PUBLIC_URL</h2>
+      <h2 className='test2'>И еще тест картинки из файла HTML встроенными стилями style=</h2>
+        <div className='test-html' style={{backgroundImage:'url(/image/119.gif)', border:'1px solid blue'}} ></div>
+
+      <h2 className='test2'>Отображение картинки с помощью process.env.PUBLIC_URL из public</h2>
         <img src={process.env.PUBLIC_URL + '/images/140.gif'}></img>
 
-      <h2 className='test2'>Отображение картинки с помощью require</h2>
+      <h2 className='test2'>Отображение картинки с помощью require из src</h2>
         <img src={require("../../image/41.gif")}></img>
 
-      <h2 className='test2'>И еще тест картинки из файла стилей CSS</h2>
+      <h2 className='test2'>И еще тест картинки из файла стилей CSS из src</h2>
         <div className='test'></div>
 
-      <h2 className='test2'>И еще тест картинки из файла HTML встроенными стилями style=</h2>
-        <div className='test-html' style={{backgroundImage:'url(/image/119.gif)'}} ></div>
-
-      <h2 className='test2'>Тест картинки если ее импортировать</h2>
+      <h2 className='test2'>Тест картинки если ее импортировать из src</h2>
         <img src={johnsonImg} />
 
     </div>
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -6,11 +6,11 @@ export const NavBar = () => {
   return (
     <nav className="nav">
       <ul className="list">
-        <li className="item"><NavLink to='/aboutme' activeClassName='active'>Админка</NavLink></li>
-        <li className="item"><NavLink to='/counter' activeClassName={styles.active}>Counter with redux</NavLink></li>
-        <li className="item"><NavLink to='/todo' activeClassName={styles.active}>To do</NavLink></li>
-        <li className="item"><NavLink to='/imagepath' activeClassName={styles.active}>Image Path</NavLink></li>
-        <li className="item"><NavLink to='/else' activeClassName={styles.active}>Else</NavLink></li>
+        <li className="item"><NavLink to='/aboutme' >Админка</NavLink></li>
+        <li className="item"><NavLink to='/counter'>Counter with redux</NavLink></li>
+        <li className="item"><NavLink to='/todo'>To do</NavLink></li>
+        <li className="item"><NavLink to='/imagepath'>Image Path</NavLink></li>
+        <li className="item"><NavLink to='/else'>Else</NavLink></li>
       </ul>
     </nav>
   );
